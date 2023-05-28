ce54d15d6c547e126389087adb79b4ec5ee00383
KadinaruDess <amreapor@up.edu.ph>
2023-05-28 21:27:43 +0800

    Timeout fix

--- api/functions/timeout.js
+++ api/functions/timeout.js
@@ -13,12 +13,15 @@ const handler = async (event,context)=>{
     return {statusCode:200,};
 }
 
-const interval = "0/30,31 * * * *"
+// 30 minute timeout interval in cron golang format
+const interval = "0/30,31 * * * *" 
+/* 
+currently runs again in the succeeding minute as an adlib solution
+to the problem that the firestore write functions don't really
+run properly in scheduled functions UNLESS it runs every minute.
+*/
 
 // Alt values for testing:
-//const interval = "* * * * *"// 1 minute interval
-//const interval = "0/2 * * * *"// 2 minute interval
-//const interval = "0 6 * * *" // 6AM daily
 
 // schedule the timeoutHandler to run every interval
 exports.handler = schedule(interval, handler);
